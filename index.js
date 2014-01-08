ced5ad173ca2aa23c85b1ef5baf850b4855cd2bb
Chris Wagner <Chris.Wagner@telerik.com>
2014-01-08 13:39:01 -0500

    Better tracking, logging during lifecycle

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var PNGReader = require('png.js');
 var fs = require('fs');
 
 var shouldTrack = false;
-var direction = "";
+var direction = null;
 
 client.getPngStream().on('data', function(data) {
   if (!shouldTrack)
@@ -49,49 +49,54 @@ client.getPngStream().on('data', function(data) {
       if (direction !== "left") {
         direction = "left";
         client.stop();
-      }
-      console.log("counter clockwise");
-      client.after(100, function() {
-        client.counterClockwise(0.2);
+
+        console.log("counter clockwise");
+        client.counterClockwise(0.1);
         client.animateLeds('frontLeftGreenOthersRed', 40, 1);
-      });
+      }
     } else if (green.right - green.left >= TURN_THRESHHOLD) {
       if (direction !== "right") {
         direction = "right";
         client.stop();
-      }
-      console.log("clockwise");
-      client.after(100, function() {
-        client.clockwise(0.2);
+
+        console.log("clockwise");
+        client.clockwise(0.1);
         client.animateLeds('frontRightGreenOthersRed', 40, 1);
-      });
-    } else {
-      direction = "";
-      if (direction !== "") {
-        direction = 0;
-        client.stop();
       }
-      console.log("stay the course");
-      client.animateLeds('blinkGreen', 15, 1);
+    } else {
+      if (direction !== "") {
+        direction = "";
+        client.stop();
+
+        console.log("stay the course");
+        client.animateLeds('blinkGreen', 15, 1);
+      }
     }
 
-    fs.writeFile('imgs/frame' + Date.now() + JSON.stringify(green) + '.png', data, function() {
+    fs.writeFile('imgs/frame' + Date.now() + JSON.stringify([green.left, green.right]) + '.png', data, function() {
     });
   });
 });
 
+console.log("takeoff");
 client.takeoff();
-
 client.after(5000, function() {
   client.on('navdata', function(d) {
     //console.log(d);
   });
-  client.up(0.15);
-  client.after(2000, function() {
+  console.log("ascend");
+  client.up(0.10);
+  client.after(3000, function() {
+    console.log("stopped ascent")
     client.stop();
-    shouldTrack = true;
-    client.after(8000, function() {
-      client.land();
+    client.after(1000, function() {
+      console.log("started tracking");
+      shouldTrack = true;
+      client.after(12000, function() {
+        console.log("landing");
+        shouldTrack = false;
+        client.land();
+      });
     });
   });
 });
