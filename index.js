09e10a2c9c2bfa7009af318af68b8811179d98f5
Chris Wagner <Chris.Wagner@telerik.com>
2014-01-08 10:45:39 -0500

    Dumps a series of PNGs

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var arDrone = require('ar-drone');
-var client = arDrone.createClient();
+var client = arDrone.createClient({
+  frameRate: 2
+});
+var fs = require('fs');
+
+var frame = 0;
+
+client.getPngStream().on('data', function(data) {
+  frame++;
+  fs.writeFile('imgs/frame' + frame + ".png", data, function() {
+  });
+});
 
 client.takeoff();
 
-// client
-//   .after(5000, function() {
-//     this.clockwise(0.5);
-//   })
-//   .after(3000, function() {
-//     this.animate('flipLeft', 15);
-//   })
-//   .after(1000, function() {
-//     this.stop();
-//     this.land();
-//   });
 client.after(5000, function() {
   client.on('navdata', function(d) {
     console.log(d);
