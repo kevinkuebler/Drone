751f42557bf65922fa00580de37dda0622924529
Kevin Kuebler <kevinkuebler@gmail.com>
2014-01-08 14:42:43 -0500

    Better chase

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var TURN_THRESHHOLD = 6;
-var FRAME_RATE = 3;
+var TURN_THRESHHOLD = 12;
+var FRAME_RATE = 10;
 
 var arDrone = require('ar-drone');
 var client = arDrone.createClient({
@@ -10,6 +10,7 @@ var fs = require('fs');
 
 var shouldTrack = false;
 var direction = null;
+var nextDirection = null;
 
 client.getPngStream().on('data', function(data) {
   if (!shouldTrack)
@@ -28,7 +29,7 @@ client.getPngStream().on('data', function(data) {
     var green = { left: 0, right: 0 };
     var pixel, r, g, b;
 
-    for (x = 0; x < w; x += 40) {
+    for (x = 0; x < w; x += 20) {
       for (y = 0; y < h; y += 30) {
         pixel = png.getPixel(x, y);
         r = pixel[0];
@@ -46,7 +47,7 @@ client.getPngStream().on('data', function(data) {
     }
 
     if (green.left - green.right >= TURN_THRESHHOLD) {
-      if (direction !== "left") {
+      if (direction !== "left" && nextDirection === "left") {
         direction = "left";
         client.stop();
 
@@ -54,8 +55,9 @@ client.getPngStream().on('data', function(data) {
         client.counterClockwise(0.14);
         client.animateLeds('leftMissile', 6, 1);
       }
+      nextDirection = "left";
     } else if (green.right - green.left >= TURN_THRESHHOLD) {
-      if (direction !== "right") {
+      if (direction !== "right" && nextDirection === "right") {
         direction = "right";
         client.stop();
 
@@ -63,23 +65,30 @@ client.getPngStream().on('data', function(data) {
         client.clockwise(0.14);
         client.animateLeds('rightMissile', 6, 1);
       }
+      nextDirection = "right";
     } else if ((green.left + green.right) > TURN_THRESHHOLD * 1.1 &&
         Math.abs(green.left - green.right) <= TURN_THRESHHOLD) {
-      if (direction !== "forwards") {
+      if (direction !== "forwards" && nextDirection === "forwards") {
         console.log("forwards");
         client.stop();
         client.front(0.2);
         direction = "forwards";
-        client.animateLeds("snakeGreenRed", 6, 1);
+        client.animateLeds("blinkRed", 6, 1);
+        client.after(100, function() {
+          client.stop();
+          direction = "";
+        });
       }
+      nextDirection = "forwards";
     } else {
-      if (direction !== "") {
+      if (direction !== "" && (nextDirection === "" || nextDirection === "forwards")) {
         direction = "";
         client.stop();
 
         console.log("stay the course");
         client.animateLeds('blinkGreen', 2, 1);
       }
+      nextDirection = "";
     }
 
     fs.writeFile('imgs/frame' + Date.now() + JSON.stringify([green.left, green.right]) + '.png', data, function() {
@@ -91,14 +100,14 @@ console.log("takeoff");
 client.takeoff(function() {
   console.log("ascend");
   client.after(2000, function() {
-    client.up(0.10);
-    client.after(3000, function() {
+    client.up(0.15);
+    client.after(2000, function() {
       console.log("stopped ascent")
       client.stop();
       client.after(0, function() {
         console.log("started tracking");
         shouldTrack = true;
-        client.after(20000, function() {
+        client.after(30000, function() {
           console.log("landing");
           shouldTrack = false;
           client.land();
